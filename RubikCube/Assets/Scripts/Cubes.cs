eb94290120847f1eb24dacd9cd85b69aaf45d319
markf06 <markfranklin346@gmail.com>
2020-04-04 16:41:02 +0100

    Deserialise Cube json into c# structure

---
+++ RubikCube/Assets/Scripts/Cubes.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using Newtonsoft.Json;
+using Scripts.Json;
+
+public class Cubes : MonoBehaviour
+{
+
+    public string cubeDefJson;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // load json into structure...
+        string path = string.Format("json/{0}", cubeDefJson);
+        TextAsset json = Resources.Load<TextAsset>(path);
+        CubeStructure cubedef = JsonConvert.DeserializeObject<CubeStructure>(json.text);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ RubikCube/Assets/Scripts/JsonObjects/CubeStructure.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json;
+
+namespace Scripts.Json
+{
+
+    [System.Serializable]
+    public class CubeStructure
+    {
+        public CubeDefinition cubedef;
+
+        [System.Serializable]
+        public class CubeDefinition
+        {
+            public Slice[] slices;
+            public Cube[] cubes;
+
+            public class Slice
+            {
+                public string Name;
+                public Position[] positions;
+
+                [System.Serializable]
+                public class Position
+                {
+                    public int Index;
+                    public bool Occupied;
+                    public string CubeAtPosition;
+                }
+            }
+
+            [System.Serializable]
+            public class Cube
+            {
+                public string Name;
+            }
+        }
+    }
+}
