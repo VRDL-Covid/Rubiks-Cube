2a78a756cd77628f947b440e035b07cfbc5d83c2
markf06 <markfranklin346@gmail.com>
2020-04-11 12:11:19 +0100

    Remain object refresh

---
+++ RubikCube/Assets/Scripts/CubeDetect.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeDetect : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(string.Format("{1} entered {0}", gameObject.name, other.gameObject.name));
+    }
+
+    private void _OnTriggerExit(Collider other)
+    {
+        Debug.Log(string.Format("{1} left {0}", gameObject.name, other.gameObject.name));
+    }
+
+    private void _OnTriggerStay(Collider other)
+    {
+        Debug.Log(string.Format("{1} staying at {0}", gameObject.name, other.gameObject.name));
+    }
+}
